71891edd4d9014afa737b91eeaf844cbc2842b42
Brian Strobach <brian@appsaurus.io>
2018-12-11 14:38:28 -0600

    Added BorderStyleable protocol

--- Sources/Shared/Styles/BorderStyle.swift
+++ Sources/Shared/Styles/BorderStyle.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import UIKitExtensions
 
 open class BorderStyle: Style{
     open var borderColor: UIColor?
@@ -20,22 +21,94 @@ open class BorderStyle: Style{
     }
 }
 
+public protocol BorderStyleable{
+    func borderStyle_() -> BorderStyle
+    func apply(borderStyle style: BorderStyle?)
+}
 
-public extension UIView{
+extension UIView: BorderStyleable{
     
     public func borderStyle_() -> BorderStyle{
         return BorderStyle(borderColor: borderColor, borderWidth: borderWidth)
     }
     
-    public func apply(borderStyle style: BorderStyle?, optimizeRendering: Bool = false){
-		guard let style = style else{
-			layer.borderWidth = 0.0
-			borderColor = nil
-			return
-		}
+    public func apply(borderStyle style: BorderStyle?){
+        guard let style = style else{
+            layer.borderWidth = 0.0
+            borderColor = nil
+            return
+        }
         borderWidth = style.borderWidth
-        borderColor = style.borderColor        
+        borderColor = style.borderColor
     }
 }
 
+extension CALayer: BorderStyleable{
+    
+    public func borderStyle_() -> BorderStyle{
+        return BorderStyle(borderColor: borderColor?.uiColor, borderWidth: borderWidth)
+    }
+    
+    public func apply(borderStyle style: BorderStyle?){
+        guard let style = style else{
+            borderWidth = 0.0
+            borderColor = nil
+            return
+        }
+        borderWidth = style.borderWidth
+        borderColor = style.borderColor?.cgColor
+    }
+}
+public protocol ShadowStyleable{
+    func apply(shadowStyle style: ShadowStyle, optimizeRendering: Bool)
+    func shadowStyle() -> ShadowStyle?
+    
+}
+extension CALayer: ShadowStyleable{
+    
+    public func apply(shadowStyle style: ShadowStyle, optimizeRendering: Bool = true){
+        shadowColor = style.shadowColor?.cgColor
+        masksToBounds = shadowColor != nil && style.shadowOpacity > 0.0 ? false : true
+        shadowOpacity = style.shadowOpacity
+        shadowOffset = style.shadowOffset
+        shadowRadius = style.shadowRadius
+        
+        //Perormance improvements for shadows
+        //        if optimizeRendering{
+        //            layer.shadowPath = UIBezierPath(rect:bounds).cgPath
+        //            self.optimizeRendering()
+        //        }
+    }
+    
+    public func shadowStyle() -> ShadowStyle?{
+        guard let shadowColor = shadowColor else { return nil }
+        return ShadowStyle(shadowColor: UIColor(cgColor: shadowColor), shadowOffset: shadowOffset, shadowOpacity: shadowOpacity, shadowRadius: shadowRadius)
+    }
+    
+}
 
+extension CALayer : ViewStyleable{
+    public func viewStyle() -> ViewStyle{
+        return ViewStyle(backgroundColor: backgroundColor?.uiColor, borderStyle: self.borderStyle_(), shadowStyle: self.shadowStyle())
+    }
+    public func apply(viewStyle style: ViewStyle, optimizeRendering: Bool = true){
+        backgroundColor = style.backgroundColor?.cgColor
+        apply(borderStyle: style.borderStyle)
+        apply(shape: style.shape)
+        guard let shadowStyle = style.shadowStyle else { return }
+        apply(shadowStyle: shadowStyle)
+    }
+    
+    public func apply(shape: ViewShape){
+        switch shape{
+        case .square:
+            cornerRadius = 0.0
+        case .roundedRect:
+            cornerRadius = ViewShape.roundedRectCornerRadius
+        case .custom(let radius):
+            cornerRadius = radius
+        case .rounded:
+            cornerRadius = self.frame.minSideLength/2.0
+        }
+    }
+}
